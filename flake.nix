707e5efc016cc2657801e84b4a0d4795bdcc8110
Dominik Schrempf <dominik.schrempf@gmail.com>
2021-08-19 14:53:34 +0200

    cabal2nix

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Circular stacks";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, flake-utils, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+
+          haskellPackages = pkgs.haskellPackages;
+
+          packageName = "circular";
+        in
+          {
+            packages.${packageName} =
+              haskellPackages.callCabal2nix packageName self rec {
+              };
+
+            defaultPackage = self.packages.${system}.${packageName};
+
+            devShell = pkgs.mkShell {
+              buildInputs = with haskellPackages; [
+                haskell-language-server
+                ghcid
+                cabal-install
+              ];
+              inputsFrom = builtins.attrValues self.packages.${system};
+            };
+          }
+    );
+}
