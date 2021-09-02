a9dfc3194bddbab043b0207973289d20366d958d
Dominik Schrempf <dominik.schrempf@gmail.com>
2021-09-02 15:20:39 +0200

    nix flake update

--- flake.nix
+++ flake.nix
@@ -5,29 +5,35 @@
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-  outputs = { self, flake-utils, nixpkgs }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-        let
-          pkgs = import nixpkgs { inherit system; };
-          haskellPackages = pkgs.haskellPackages;
-          circular = haskellPackages.callCabal2nix "circular" self rec {};
-          circular-dev = pkgs.haskell.lib.doBenchmark circular;
-        in
-          {
-            packages.circular = circular;
+  outputs =
+    { self
+    , flake-utils
+    , nixpkgs
+    }:
+      flake-utils.lib.eachDefaultSystem (
+        system:
+          let
+            pkgs = import nixpkgs { inherit system; };
+            haskellPackages = pkgs.haskellPackages;
+            circular = haskellPackages.callCabal2nix "circular" self rec {};
+            circular-dev = pkgs.haskell.lib.doBenchmark circular;
+          in
+            {
+              packages.circular = circular;
 
-            defaultPackage = circular;
+              defaultPackage = circular;
 
-            devShell = pkgs.haskellPackages.shellFor {
-              packages = _: [ circular-dev ];
-              buildInputs = with pkgs; [
-                haskellPackages.cabal-install
-                haskellPackages.haskell-language-server
-                haskellPackages.stack
-              ];
-              doBenchmark = true;
-            };
-          }
-    );
+              devShell = pkgs.haskellPackages.shellFor {
+                packages = _: [ circular-dev ];
+                buildInputs = with pkgs; [
+                  bashInteractive
+                  haskellPackages.cabal-install
+                  haskellPackages.haskell-language-server
+                  haskellPackages.stack
+                ];
+                doBenchmark = true;
+                withHoogle = true;
+              };
+            }
+      );
 }
