cb17aea6b5099617da93c6d121995956c3ec338b
Dominik Schrempf <dominik.schrempf@gmail.com>
2022-02-17 17:38:43 +0100

    ghc 921

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
       system:
       let
         pkgs = import nixpkgs { inherit system; };
-        hpkgs = pkgs.haskellPackages;
+        hpkgs = pkgs.haskell.packages.ghc921;
         circular = hpkgs.callCabal2nix "circular" self rec { };
         circular-dev = pkgs.haskell.lib.doBenchmark circular;
       in
