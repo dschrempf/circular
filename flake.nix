8b03f4ed05dae8b36c952ff31b8d368a5b8f11f5
Dominik Schrempf <dominik.schrempf@gmail.com>
2022-12-13 21:13:41 +0100

    update toolign (ghc943)

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Circular stacks";
+  description = "Haskell development environment";
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
@@ -10,29 +10,60 @@
     , flake-utils
     , nixpkgs
     }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        hpkgs = pkgs.haskell.packages.ghc924;
-        circular = hpkgs.callCabal2nix "circular" self rec { };
-        circular-dev = pkgs.haskell.lib.doBenchmark circular;
-      in
-      {
-        packages.default = circular;
-
-        devShells.default = hpkgs.shellFor {
-          packages = _: [ circular-dev ];
-          buildInputs = with pkgs; [
-            bashInteractive
-
-            hpkgs.cabal-fmt
-            hpkgs.cabal-install
-            hpkgs.haskell-language-server
-          ];
-          doBenchmark = true;
-          # withHoogle = true;
+    let
+      theseHpkgNames = [
+        "circular"
+      ];
+      thisGhcVersion = "ghc943";
+      # # Only required for projects with multiple packages.
+      # hMkPackage = h: n: h.callCabal2nix n (./. + "/${n}") { };
+      hOverlay = selfn: supern: {
+        haskell = supern.haskell // {
+          packageOverrides = selfh: superh:
+            supern.haskell.packageOverrides selfh superh //
+              {
+                circular = selfh.callCabal2nix "circular" ./. { };
+              };
         };
-      }
-    );
+      };
+      perSystem = system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ hOverlay ];
+          };
+          hpkgs = pkgs.haskell.packages.${thisGhcVersion};
+          hlib = pkgs.haskell.lib;
+          theseHpkgs = nixpkgs.lib.genAttrs theseHpkgNames (n: hpkgs.${n});
+          theseHpkgsDev = builtins.mapAttrs (_: x: hlib.doBenchmark x) theseHpkgs;
+        in
+        {
+          packages = theseHpkgs // { default = theseHpkgs.circular; };
+
+          devShells.default = hpkgs.shellFor {
+            # shellHook =
+            #   let
+            #     scripts = ./scripts;
+            #   in
+            #   ''
+            #     export PATH="${scripts}:$PATH"
+            #   '';
+            packages = _: (builtins.attrValues theseHpkgsDev);
+            nativeBuildInputs = with pkgs; [
+              # See https://github.com/NixOS/nixpkgs/issues/59209.
+              bashInteractive
+
+              # Haskell toolchain.
+              hpkgs.cabal-fmt
+              hpkgs.cabal-install
+              hpkgs.haskell-language-server
+            ];
+            buildInputs = with pkgs; [
+            ];
+            doBenchmark = true;
+            # withHoogle = true;
+          };
+        };
+    in
+    { overlays.default = hOverlay; } // flake-utils.lib.eachDefaultSystem perSystem;
 }
